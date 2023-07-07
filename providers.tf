b0bc3ec48f318946e99669dca04edd699b3087ab
kumarvangapally <67318450+kumarvangapally@users.noreply.github.com>
2023-07-07 10:53:27 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,9 +2,7 @@ provider "aws" {
   region = "ap-south-1"
   access_key="AKIASMZJBWS6QKJWX7OV"
   secret_key="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"
-  assume_role{
-  role_arn = "arn:aws:iam::164905465021:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS"  
-}
+ 
 }
 
 data "aws_region" "current" {
