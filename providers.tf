f61826c5b4d69c982bfdfa167f641bfedf0648e4
kumarvangapally <67318450+kumarvangapally@users.noreply.github.com>
2023-07-07 09:46:29 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "ap-south-1"
+  assume_role{
+  role_arn = "arn:aws:iam::164905465021:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS"
+}
 }
 
 data "aws_region" "current" {
